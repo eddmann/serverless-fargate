8cad3b702ec76b98181e1e902c91803a5696c246
Ryan Gannon <67928391+rpgannon@users.noreply.github.com>
2022-08-02 15:38:32 -0700

    Add config to control log retention

--- src/compiler.js
+++ src/compiler.js
@@ -34,6 +34,7 @@ const compileCluster = config => ({
       Type: 'AWS::Logs::LogGroup',
       Properties: {
         LogGroupName: config.logGroupName,
+        RetentionInDays: config.logRetentionInDays,
         Tags: toTags(config.tags),
       },
     },
