258137493f9ce6b3fec92dd463487be3baa246b9
Ryan Gannon <67928391+rpgannon@users.noreply.github.com>
2022-08-02 14:54:14 -0700

    add support for dependsOn

--- src/parser.js
+++ src/parser.js
@@ -5,6 +5,7 @@ const { get } = require('./util');
 const parseTask = (global, name, task) => {
   const definition = {
     name: task.name || name,
+    dependsOn: task.dependsOn || [],
     image: task.image,
     executionRoleArn: task.executionRoleArn || global.executionRoleArn,
     taskRoleArn: task.taskRoleArn || global.taskRoleArn,
