ab7712dc76cfef99b45e17890f12b8bbd9d4631f
Edd Mann <the@eddmann.com>
2022-08-22 22:29:42 +0100

    Ensure both strings and objects accepted as ARNs
    This allows for `!GetAttr` support within the configuration

--- src/schema.js
+++ src/schema.js
@@ -7,10 +7,16 @@ module.exports = {
     cpu: { type: 'integer', enum: [256, 512, 1024, 2048, 4096] },
     architecture: { type: 'string', enum: ['X86_64', 'ARM64'] },
     environment: { type: 'object' },
-    executionRoleArn: { type: 'object' },
-    taskRoleArn: { type: 'object' },
+    executionRoleArn: { type: ['object', 'string'] },
+    taskRoleArn: { type: ['object', 'string'] },
     logGroupName: { type: 'string' },
-    logRetentionInDays: { type: 'integer', enum: [1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 2192, 2557, 2922, 3288, 3653] },
+    logRetentionInDays: {
+      type: 'integer',
+      enum: [
+        1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827,
+        2192, 2557, 2922, 3288, 3653,
+      ],
+    },
     iamRoleStatements: { type: 'array' },
     iamManagedPolicies: { type: 'array', items: { type: 'string' } },
     vpc: {
@@ -35,8 +41,8 @@ module.exports = {
           properties: {
             name: { type: 'string' },
             image: { type: 'string' },
-            executionRoleArn: { type: 'object' },
-            taskRoleArn: { type: 'object' },
+            executionRoleArn: { type: ['object', 'string'] },
+            taskRoleArn: { type: ['object', 'string'] },
             vpc: {
               type: 'object',
               properties: {
